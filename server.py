1e6b986a900516b0b1b646e2707741cbb94d6e66
Sergiy Sanin <sanin_sergiy@yahoo.com>
2018-02-10 17:40:14 +0800

    request/response framework

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-import time
+import socket
+import logging
 
+logging.basicConfig(format='%(name)s %(asctime)-15s %(message)s')
+server_host, server_port = 'localhost', 50000
 
 class QuoteCalculationEngine:
     def calculate_quote_price(self, security_id, reference_price, buy, quantity):
@@ -20,6 +23,23 @@ class ReferencePriceSourceListener:
 
 
 if "__main__" == __name__:
-    while True:
-        print(123)
-        time.sleep(1)
+    log = logging.getLogger('SERVER')
+    log.setLevel(logging.DEBUG)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    log.info('Binding to host %s, port %d' % (server_host, server_port))
+    s.bind((server_host, server_port))
+    s.listen(1)
+    conn, addr = s.accept()
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        str_data = data.decode()
+        log.debug('Received request: %s' % str_data)
+        if 'STOP' == str_data:
+            log.info('Stopping...')
+            break
+        # TODO send response
+        conn.sendall(data)
+    conn.close()
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,29 +1,43 @@
 import unittest
 import subprocess
-import time
-import os
 import sys
+import socket
+import time
+import logging
+
+logging.basicConfig(format='%(name)s %(asctime)-15s %(message)s')
+server_host, server_port = 'localhost', 50000
+
+
+class TestClient:
+    def __init__(self):
+        self.log = logging.getLogger('CLIENT')
+        self.log.setLevel(logging.DEBUG)
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((server_host, server_port))
+        # XXX no self.conn.close() call
+
+    def get(self, security_id, is_buy, quantity):
+        req = '%d %s %d' % (security_id, 'BUY' if is_buy else 'SELL', quantity)
+        self.log.info('Requesting: ' + req)
+        self.conn.sendall(req.encode())
+        data = self.conn.recv(1024)
+        print('Received', repr(data))
 
 
 class TestServer(unittest.TestCase):
     def setUp(self):
-        #print(os.getcwd())
-        self.server = subprocess.Popen(['python', 'server.py'], stdout=sys.stdout)
+        self.server = subprocess.Popen(['python', 'server.py'], stdout=sys.stdout, stderr=sys.stdout)
+        time.sleep(0.1)  # XXX can be a better way to make sure the server is already listening for connections
 
     def tearDown(self):
+        # TODO send stop signal
         self.server.kill()
 
     def test_simple1(self):
+        c = TestClient()
+        c.get(1, True, 3)
         self.assertTrue(True)
-        time.sleep(3)
-
-    def test_simple2(self):
-        self.assertTrue(True)
-        time.sleep(3)
-
-    def test_simple3(self):
-        self.assertTrue(True)
-        time.sleep(3)
 
 
 if '__main__' == __name__:
